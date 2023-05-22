816684bf58f2cdfc77f8d592539d8f36e12daa94
vitor.silva <vitor2071@gmail.com>
2023-05-22 20:15:09 -0300

    feat: cria arquivo sobre variáveis e listas

---
+++ variaveis.dart
@@ -0,0 +1,36 @@
+void main() { 
+
+  int idade = 25; 
+  double altura = 1.86; 
+  
+}
+
+
+/*Em Dart, as variáveis são elementos fundamentais para armazenar e manipular dados durante a execução de um programa. Elas são usadas para guardar valores que podem ser modificados ao longo do código, permitindo uma maior flexibilidade e dinamismo na programação.
+
+Existem diferentes tipos de variáveis em Dart, sendo os principais: inteiros (int), números de ponto flutuante (double), booleanos (bool), strings (String) e listas (List). Cada tipo de variável possui características específicas e é utilizado de acordo com o tipo de dado que se deseja armazenar.
+
+Ao declarar uma variável em Dart, é necessário especificar o seu tipo, seguido pelo nome que será atribuído a ela. Por exemplo:
+
+arduino
+Copy code
+int idade = 25;
+double altura = 1.75;
+bool isAtivo = true;
+String nome = "João";
+List<int> numeros = [1, 2, 3, 4, 5];
+
+
+Neste exemplo, criamos variáveis com diferentes tipos e atribuímos valores a elas. A variável "idade" é do tipo inteiro e recebe o valor 25. A variável "altura" é do tipo ponto flutuante e recebe o valor 1.75. A variável "isAtivo" é do tipo booleano e recebe o valor true. A variável "nome" é do tipo string e recebe o valor "João". A variável "numeros" é do tipo lista de inteiros e recebe uma sequência de números.
+
+Além disso, em Dart, é possível alterar o valor de uma variável ao longo do programa, simplesmente atribuindo um novo valor a ela. Por exemplo:
+
+makefile
+Copy code
+idade = 26;
+nome = "Maria";
+Neste caso, atualizamos o valor da variável "idade" para 26 e o valor da variável "nome" para "Maria".
+
+As variáveis em Dart também podem ser utilizadas em expressões e operações matemáticas, possibilitando realizar cálculos e manipulações de dados. Além disso, é possível realizar a leitura e escrita de variáveis por meio da interação com o usuário, utilizando comandos de entrada e saída.
+
+Em resumo, as variáveis são elementos essenciais em Dart para armazenar e manipular dados durante a execução de um programa. Elas permitem armazenar diferentes tipos de dados e são atualizáveis ao longo do código, proporcionando flexibilidade e dinamismo na programação. */
